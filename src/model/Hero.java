f67ba5d87d82c2f5ee22eb21b0c1a53a88d690a4
Aziz <abazizguido@gmail.com>
2019-10-20 13:04:00 +0200

    Implementation Hero

---
+++ src/model/Hero.java
@@ -0,0 +1,43 @@
+package model;
+
+public class Hero {
+    private int x;
+    private int y;
+
+    public Hero(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    private void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    private void setY(int y) {
+        this.y = y;
+    }
+
+    public void goUp(){
+        this.setY(y--);
+    }
+
+    public void goDown(){
+        this.setY(y++);
+    }
+
+    public void goLeft(){
+        this.setX(x--);
+    }
+
+    public void goRight(){
+        this.setX(x++);
+    }
+}
