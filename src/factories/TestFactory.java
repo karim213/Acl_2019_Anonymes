05721fd6b1f2ff61d0c706430a2f121b004e0454
karim <karim.abdi9@etu.univ-lorraine.fr>
2019-11-05 16:06:05 +0100

    creation labyrinth

--- src/factories/TestFactory.java
+++ src/factories/TestFactory.java
@@ -20,23 +20,80 @@ public class TestFactory {
 
             Walls walls = new Walls();
             walls.addWall(22, 20);
-            walls.addWall(22, 24);
+            walls.addWall(24, 20);
             walls.addWall(26, 20);
+            walls.addWall(28, 20);
             walls.addWall(30, 20);
+            walls.addWall(32, 20);
+            walls.addWall(34, 20);
+            walls.addWall(36, 20);
+
+
+            walls.addWall(22, 24);
+            walls.addWall(36, 24);
+
+            walls.addWall(22, 24);
+            walls.addWall(36, 24);
 
             walls.addWall(22, 28);
+            walls.addWall(36, 28);
 
-            walls.addWall(30, 30);
-            walls.addWall(40, 40);
+            walls.addWall(22, 32);
+            walls.addWall(36, 32);
 
-            Labyrinthe labyrinthe = new Labyrinthe(80 , 80, new Hero(20, 10), walls, new Chest(new Position(20, 35)));
+            walls.addWall(22, 34);
+            walls.addWall(36, 34);
+
+            walls.addWall(22, 34);
+            walls.addWall(36, 34);
+
+            walls.addWall(22, 38);
+            walls.addWall(36, 38);
+
+            walls.addWall(22, 42);
+            walls.addWall(36, 42);
+
+
+            walls.addWall(22, 46);
+            walls.addWall(36, 46);
+
+
+            walls.addWall(22, 48);
+            walls.addWall(36, 48);
+
+
+            walls.addWall(22, 52);
+            walls.addWall(24, 52);
+            walls.addWall(26, 52);
+            walls.addWall(28, 52);
+            walls.addWall(32, 52);
+            walls.addWall(34, 52);
+            walls.addWall(36, 52);
+
+
+            Labyrinthe labyrinthe = new Labyrinthe(80 , 80, new Hero(10, 10), walls, new Chest(new Position(25, 25)));
 
             Enemies enemies = new Enemies();
             EnemyMovementStrategy strategy = new RandomMovement();
-            enemies.addEnemie(new Monster(30, 39,labyrinthe));
-            enemies.addEnemie(new Monster(35, 30,labyrinthe));
-            enemies.addEnemie(new Monster(10, 10,labyrinthe));
-            enemies.addEnemie(new Monster(9, 25,labyrinthe));
+            enemies.addEnemie(new Monster(28, 44,labyrinthe));
+            enemies.addEnemie(new Monster(31, 42,labyrinthe));
+            enemies.addEnemie(new Monster(26, 29,labyrinthe));
+            enemies.addEnemie(new Monster(30, 30,labyrinthe));
+
+
+            enemies.addEnemie(new Monster(28, 60,labyrinthe));
+            enemies.addEnemie(new Monster(38, 36,labyrinthe));
+            enemies.addEnemie(new Monster(42, 41,labyrinthe));
+            enemies.addEnemie(new Monster(60, 60,labyrinthe));
+
+
+            enemies.addEnemie(new Monster(10, 60,labyrinthe));
+            enemies.addEnemie(new Monster(63, 10,labyrinthe));
+            enemies.addEnemie(new Monster(26, 12,labyrinthe));
+            enemies.addEnemie(new Monster(39, 38,labyrinthe));
+
+
+
             enemies.setStrategy(strategy);
             labyrinthe.setEnemies(enemies);
 
